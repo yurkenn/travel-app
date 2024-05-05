bab9015c32127390cd77731eb91e32053935baeb
oğuz yürken <69719126+yurkenn@users.noreply.github.com>
2024-05-05 21:47:51 +0300

    feat: Add @tanstack/react-query for data fetching and caching

--- App.jsx
+++ App.jsx
@@ -1,7 +1,15 @@
 import 'react-native-gesture-handler';
 
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+
 import RootStack from './navigation';
 
 export default function App() {
-  return <RootStack />;
+  const queryClient = new QueryClient();
+
+  return (
+    <QueryClientProvider client={queryClient}>
+      <RootStack />
+    </QueryClientProvider>
+  );
 }
