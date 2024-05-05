af2caa8b4948ae078989c374a9a6ac42d9e3bbca
oğuz yürken <69719126+yurkenn@users.noreply.github.com>
2024-05-06 00:39:26 +0300

    feat: Update Header component with new design and image

--- components/Header.js
+++ components/Header.js
@@ -1,14 +1,34 @@
-import { StyleSheet, Text, View } from 'react-native';
+import { wp } from 'helpers/common';
 import React from 'react';
+import { Image, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 
 const Header = () => {
   return (
-    <View>
-      <Text>Header</Text>
+    <View style={styles.headerContainer}>
+      <Text style={styles.headerText}>Let's Discover</Text>
+      <TouchableOpacity>
+        <Image source={require('../assets/welcome.jpg')} style={styles.headerImage} />
+      </TouchableOpacity>
     </View>
   );
 };
 
 export default Header;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  headerContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    marginHorizontal: 20,
+  },
+  headerText: {
+    fontSize: wp(7),
+    fontWeight: 'bold',
+    color: '#4d4d4d', // Adjust color as needed
+  },
+  headerImage: {
+    height: wp(12),
+    width: wp(12),
+  },
+});
