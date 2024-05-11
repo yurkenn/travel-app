714ad91c43b252013fb95613afb77211d901b9a6
oğuz yürken <69719126+yurkenn@users.noreply.github.com>
2024-05-11 22:24:41 +0300

    refactor: Remove useState import from Options component

--- components/Options.js
+++ components/Options.js
@@ -1,5 +1,5 @@
 import { wp } from 'helpers/common';
-import React, { useState } from 'react';
+import React from 'react';
 import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 
 const Options = ({ activeOption, setActiveOption }) => {
