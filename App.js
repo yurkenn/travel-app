bebd1ce2dd0f50058443b3d41d4838ae0faca33f
oğuz yürken <69719126+yurkenn@users.noreply.github.com>
2024-05-13 01:39:32 +0300

    refactor: Update App.js to include BottomSheetModalProvider for improved user experience

--- App.js
+++ App.js
@@ -1,4 +1,5 @@
 import 'react-native-gesture-handler';
+import { BottomSheetModalProvider } from '@gorhom/bottom-sheet';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import * as SplashScreen from 'expo-splash-screen';
 import { StatusBar } from 'expo-status-bar';
@@ -21,8 +22,10 @@ export default function App() {
 
   return (
     <QueryClientProvider client={queryClient}>
-      <StatusBar />
-      <RootStack />
+      <BottomSheetModalProvider>
+        <StatusBar />
+        <RootStack />
+      </BottomSheetModalProvider>
     </QueryClientProvider>
   );
 }
